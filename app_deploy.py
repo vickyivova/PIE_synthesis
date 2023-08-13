6170daf9d93cfa2e510e1f68885e4304414a9f0c
Viktoria Ivanova V.Ivanova.4@student.rug.nl <v.ivanova.4@student.rug.nl>
2023-08-13 21:32:53 +0200

    save audios from app

--- app_deploy.py
+++ app_deploy.py
@@ -29,8 +29,11 @@ def set_png_as_page_bg(png_file):
 set_png_as_page_bg("/mount/src/pie_synthesis/app_background.png")
 
 st.title("Synthesise PIE")
-st.write("Bring to life the ancient extinct language from which most European languages evolved.\n Write down a Proto-Indo-European sentence in PIE standard notation or choose one of the sample sentences. You can choose the model behind the synthesis: one trained on European languages (pre-trained) or one further fine-tuned on Abkhaz (fine-tuned). Hit 'Synthesise' and wait for your synthesised audio!")
+st.write("Bring to life the ancient extinct language from which most European languages evolved.\n Write down a Proto-Indo-European sentence in PIE standard notation or try the sample sentence. You can choose the model behind the synthesis: one trained on European languages (pre-trained) or one further fine-tuned on Abkhaz (fine-tuned). Hit 'Synthesise' and wait for your synthesised audio!")
 
+
+user_input = st.text_input("Enter PIE text:", "Tór h₃rēǵs h₁ést. Só h₂népotlos h₁ést. Só h₃rēǵs suHnúm welh₁t.")
+  
 file_name = "synth_app_audios/default_synth_web_app.wav"
 
 selected_model = st.radio("Select which model to use: ", ("Pre-trained model", "Fine-tuned model"))
@@ -41,26 +44,16 @@ elif selected_model == "Fine-tuned model":
     model_input = "MultiAbkhaz"
 else:
     model_input = "MultiAbkhaz"
-
-
-user_input = ""
-button_input = ""
-if st.button("The King and the God poem"):
-  button_input = "Tór h₃rēǵs h₁ést. Só h₂népotlos h₁ést. Só h₃rēǵs suHnúm welh₁t."
-if st.button("Your daughter has come home."):
-  button_input = "Dʰugh₂tēr toi dōm gʷegʷome."
-
-if button_input==False:
-  user_input = st.text_input("Enter PIE text:")
-elif button_input==True:
-  user_input = button_input
-  
-if st.button("Synthesise"):
-  synthesized_text = custom_app(file_name, user_input, model_id=model_input)
-  st.success("Text synthesised!")
-  audio_file = open(file_name, "rb")
-  audio_bytes  = audio_file.read()
-  st.audio(audio_bytes, format="audio/wav")
+    
+if user_input: 
+  if st.button("Synthesise"):
+    synthesized_text = custom_app(file_name, user_input, model_id=model_input)
+    st.success("Text synthesised!")
+    audio_file = open(file_name, "rb")
+    audio_bytes  = audio_file.read()
+    st.audio(audio_bytes, format="audio/wav")
+else:
+  st.warning("Please input valid PIE text.")
 
 
 
