87049c1197402809c941d36797453a8a5fea0e1f
Viktoria Ivanova V.Ivanova.4@student.rug.nl <v.ivanova.4@student.rug.nl>
2023-08-15 11:30:41 +0200

    setting the theme to light

--- app_deploy.py
+++ app_deploy.py
@@ -33,8 +33,10 @@ def set_png_as_page_bg(png_file):
 
 set_png_as_page_bg("/mount/src/pie_synthesis/app_background.png")
 
+[theme]
+base="light"
 
-st.set_page_config(page_title="Synthesise PIE", page_icon=":smiley:", layout="wide", theme="light")
+st.title("Synthesise PIE")
 st.write("Bring to life the ancient extinct language from which most European languages evolved.\n Write down a Proto-Indo-European sentence in PIE standard notation or try the sample sentence. You can choose the model behind the synthesis: one trained on European languages (pre-trained) or one further fine-tuned on Abkhaz (fine-tuned). Hit 'Synthesise' and wait for your synthesised audio!")
 
 #Text input
